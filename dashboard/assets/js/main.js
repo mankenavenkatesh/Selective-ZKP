88969ee317c1a711865abfe89384dca30075399f
sarangparikh22 <sarangparikh22@gmail.com>
2019-08-03 08:12:53 +0530

    API calls from Frontend

--- dashboard/assets/js/main.js
+++ dashboard/assets/js/main.js
@@ -5,5 +5,15 @@ let amount;
 function sendTx(){
     accountAddress = document.getElementById('accountAddress').value;    
     amount = document.getElementById('amount').value;
-    //call API with accountAddress and Amount    
+    //call API with accountAddress and Amount
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "http://172.16.17.16:3000/sendConfidentialTransaction";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.onload() = function (){
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log(xmlhttp);
+        }
+    }
+    xmlhttp.send(JSON.stringify({ "receiver": "0x87897m", "value": amount }));
 }
