d88c20b22d50c9746f4137bdbc123c5cd60c58b3
sarangparikh22 <sarangparikh22@gmail.com>
2019-08-03 08:16:18 +0530

    Fixed Errors in Frontend Call

--- dashboard/assets/js/main.js
+++ dashboard/assets/js/main.js
@@ -10,9 +10,9 @@ function sendTx(){
     var theUrl = "http://172.16.17.16:3000/sendConfidentialTransaction";
     xmlhttp.open("POST", theUrl);
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xmlhttp.onload() = function (){
+    xmlhttp.onload = function (){
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            console.log(xmlhttp);
+            alert("Confidential Transaction Executed");
         }
     }
     xmlhttp.send(JSON.stringify({ "receiver": "0x87897m", "value": amount }));
